d1428615ce4298e9f96ca9775582ff69ac8dd7db
Yegor <kazurovegor@gmail.com>
2019-02-25 11:40:11 +0500

    landingPage

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.header').height($(window).height());
+})
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+landingPage(bootstrap4)
